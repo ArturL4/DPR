bf42e2cca0ec3a2425a9f18096aa391c7e6641b6
A <st185327@stud.uni-stuttgart.de>
2023-06-07 21:08:47 +0200

    Create pages

---
+++ streamlit/app/Detection_and_Pattern_Recognition.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.header("Detection and Pattern Recognition")
+st.text("""This Project is used to concentrate and visualize learned things\n
+from the lecture of Professor Yang from the university of Stuttgart""")
---
+++ streamlit/app/pages/1_Introduction.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.subheader("1.4 Confusion matrix")
+
+st.text("A confusion matrix is used to measure the performance of a detector/classifier")
