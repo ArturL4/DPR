51159a4dc5b2e426e99600e22bd8b3753902c0af
A <st185327@stud.uni-stuttgart.de>
2023-06-12 09:26:35 +0200

    Exc

---
+++ streamlit/app/pages/7_Excercises.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+
+
+st.header("Excercises")
+st.markdown("---")
+#############################
+st.subheader("Bayes decision theory")
+st.markdown("#### Fruit sorter")
+st.markdown(
+    r"""
+    We got 3 classes:
+        - $\omega_1:$ Banana $\Rightarrow P(\omega_1) = 0.3$
+        - $\omega_1:$ Banana $\Rightarrow P(\omega_1) = 0.3$ 
+        - $\omega_1:$ Banana $\Rightarrow P(\omega_1) = 0.3$    
+""")
